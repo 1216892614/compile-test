c924d98b188ecd6ca35147112da728fb87326eb3
Nerd_hnd <hndppt@outlook.com>
2022-04-25 15:37:03 +0800

    Add a line of comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod lexical_analysis;
 
+//TODO 为了防止cargo报错说我的词法分析没有使用而声明的函数
 use lexical_analysis::{err::LexicalAnalysisResult, lexical_analysis, KeyType};
 pub fn compile(code: &str, key_words_json: &str) -> LexicalAnalysisResult<Vec<KeyType>> {
     lexical_analysis(code, key_words_json)
